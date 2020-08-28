79f2b473f765ca9ccea28d8c77b90ea50acdde06
kevin_f_liu <1663531467@qq.com>
2020-08-29 00:51:39 +0800

    [refactor homework] [kevin] refactor: merge console.log

--- homework/src/print.js
+++ homework/src/print.js
@@ -1,8 +1,5 @@
 function printOwing(invoice) {
   let outstanding = 0;
-  console.log('***********************');
-  console.log('**** Customer Owes ****');
-  console.log('***********************');
 
   // calculate outstanding
   for (const o of invoice.borderSpacing) {
@@ -14,9 +11,6 @@ function printOwing(invoice) {
   invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 
   // print details
-  console.log(`name: ${invoice.customer}`);
-  console.log(`amount: ${outstanding}`);
-  console.log(`amount: ${invoice.dueDate.toLocaleDateString()}`);
   let result = '***********************\n' +
     '**** Customer Owes ****\n' +
     '***********************\n' +
