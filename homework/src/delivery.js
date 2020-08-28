efd93e0857ad4171e9f7deb34066e627b6234f49
kevin_f_liu <1663531467@qq.com>
2020-08-29 01:15:32 +0800

    [refactor homework] [kevin] refactor: extract getDaysWithIsRushIsFalse

--- homework/src/delivery.js
+++ homework/src/delivery.js
@@ -3,28 +3,32 @@ function deliveryDate (anOrder, isRush) {
     return getDaysWithIsRushIsTrue(anOrder);
   }
   else {
-    let deliveryTime;
-    if ([
-      'MA',
-      'CT',
-      'NY',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
-    }
-    else if ([
-      'ME',
-      'NH',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 3;
-    }
-    else {
-      deliveryTime = 4;
-    }
-    return anOrder.placedOn.plusDays(2 + deliveryTime);
+    return getDaysWithIsRushIsFalse(anOrder);
   }
 }
 
 module.exports = {deliveryDate};
+function getDaysWithIsRushIsFalse(anOrder) {
+  let deliveryTime;
+  if ([
+    'MA',
+    'CT',
+    'NY',
+  ].includes(anOrder.deliveryState)) {
+    deliveryTime = 2;
+  }
+  else if ([
+    'ME',
+    'NH',
+  ].includes(anOrder.deliveryState)) {
+    deliveryTime = 3;
+  }
+  else {
+    deliveryTime = 4;
+  }
+  return anOrder.placedOn.plusDays(2 + deliveryTime);
+}
+
 function getDaysWithIsRushIsTrue(anOrder) {
   let deliveryTime;
   if ([
