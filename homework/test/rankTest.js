a88c3c7471354cd9c0fb054ef06abd28af850ee3
kevin_f_liu <1663531467@qq.com>
2020-08-28 12:39:47 +0800

    [refactor homework] [kevin] :add project

---
+++ homework/test/rankTest.js
@@ -0,0 +1,11 @@
+const rankTest = require('ava');
+const { voyageRisk,captainHistoryRisk, voyageProfitFactor,rating } = require('../src/rank');
+// rankTest('foo', t => {
+//   t.pass();
+// });
+
+// rankTest('bar', async t => {
+//   const bar = Promise.resolve('bar');
+//   t.is(await bar, 'bar');
+// });
+
