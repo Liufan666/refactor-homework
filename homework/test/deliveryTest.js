5989ae269144f710572fcb80e244a5f237d95969
kevin_f_liu <1663531467@qq.com>
2020-08-28 22:56:49 +0800

    [refactor homework] [kevin] test: isRush is false

--- homework/test/deliveryTest.js
+++ homework/test/deliveryTest.js
@@ -70,3 +70,17 @@ deliveryTest('anOrder.deliveryState includes ME and isRush is false', t => {
     const result = deliveryDate(anOrder,isRush)
     t.is(result,5)
 })
+
+deliveryTest('isRush is false', t => {
+    const anOrder = {
+        deliveryState:'KK',
+        placedOn: {
+            plusDays: function (day) {
+                return day;
+            }
+        }
+    }
+    const isRush = false
+    const result = deliveryDate(anOrder,isRush)
+    t.is(result,6)
+})
